2a93018ae638968013981da947e3545c040f49c5
Yccc46 <chun040406@gmail.com>
2024-08-24 01:36:56 +0800

    modify

--- NewVersion/css/cart.aspx.cs
+++ NewVersion/css/cart.aspx.cs
@@ -13,7 +13,6 @@ namespace NewVersion.css
         string cs = Global.CS;
         protected void Page_Load(object sender, EventArgs e)
         {
-            Page.MaintainScrollPositionOnPostBack = true;
 
             if (!IsPostBack)
             {
