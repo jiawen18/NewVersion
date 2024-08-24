c754d7b8535219a0ba503e957a6b6c5090df7213
Chong391 <kelvinchong0457@gmail.com>
2024-08-24 18:48:04 +0800

    new update

--- NewVersion/css/login.aspx.designer.cs
+++ NewVersion/css/login.aspx.designer.cs
@@ -103,5 +103,14 @@ namespace NewVersion.css
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton login_google;
+
+        /// <summary>
+        /// login_facebook control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton login_facebook;
     }
 }
