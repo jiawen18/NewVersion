1da9bfde5a09a8741dc59ded55490f2c1ba04bcf
Chong391 <kelvinchong0457@gmail.com>
2024-08-25 17:47:40 +0800

    solve

--- NewVersion/Home.Master.designer.cs
+++ NewVersion/Home.Master.designer.cs
@@ -42,15 +42,6 @@ namespace NewVersion
         protected global::System.Web.UI.WebControls.Menu Menu1;
 
         /// <summary>
-        /// nav-login control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink nav-login;
-        
-        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
