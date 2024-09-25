f59373fb94e4f756d7074117a6c2ca16ae28f7ef
jiawen18 <162662193+jiawen18@users.noreply.github.com>
2024-09-26 01:00:40 +0800

    product

--- NewVersion/css/Smartphones.aspx.cs
+++ NewVersion/css/Smartphones.aspx.cs
@@ -28,7 +28,9 @@ namespace NewVersion.css
         private void LoadProducts()
         {
             List<Product> products = new List<Product>();
-            string query = "SELECT ProductID, ProductName, ProductImageURL, Price FROM Product WHERE IsVisible = 1";
+            string query = "SELECT P.ProductID, P.ProductName, P.ProductImageURL, P.Price, R.ReviewID, R.ReviewDate, R.ReviewRating, R.ReviewDescription " +
+                           "FROM Product P LEFT JOIN Review R ON P.ProductID = R.ProductID WHERE P.IsVisible = 1";
+
 
             using (SqlConnection con = new SqlConnection(connectionString))
             {
@@ -38,22 +40,73 @@ namespace NewVersion.css
 
                 while (reader.Read())
                 {
-                    // Create a product object and populate it
-                    var product = new Product
+                    // Check if the product is already added
+                    var existingProduct = products.Find(p => p.ProductID == reader["ProductID"].ToString());
+
+                    if (existingProduct == null)
                     {
-                        ProductID = reader["ProductID"].ToString(),
-                        ProductName = reader["ProductName"].ToString(),
-                        ProductImageURL = reader["ProductImageURL"].ToString(),
-                        Price = Convert.ToDecimal(reader["Price"]),
-                       
-                    };
-                    products.Add(product);
+                        // Create a new product if it's not already added
+                        var product = new Product
+                        {
+                            ProductID = reader["ProductID"].ToString(),
+                            ProductName = reader["ProductName"].ToString(),
+                            ProductImageURL = reader["ProductImageURL"].ToString(),
+                            Price = Convert.ToDecimal(reader["Price"]),
+                            Reviews = new List<Review>() // Initialize reviews list
+                        };
+
+                        // If there's a review, add it to the list of reviews
+                        if (!DBNull.Value.Equals(reader["ReviewID"]))
+                        {
+                            var review = new Review
+                            {
+                                ReviewID = reader["ReviewID"].ToString(),
+                                ReviewDate = Convert.ToDateTime(reader["ReviewDate"]),
+                                ReviewRating = Convert.ToInt32(reader["ReviewRating"]),
+                                ReviewDescription = reader["ReviewDescription"].ToString()
+                            };
+                            product.Reviews.Add(review);
+                        }
+
+                        products.Add(product);
+                    }
+                    else
+                    {
+                        // If product exists, add the review to the existing product
+                        if (!DBNull.Value.Equals(reader["ReviewID"]))
+                        {
+                            var review = new Review
+                            {
+                                ReviewID = reader["ReviewID"].ToString(),
+                                ReviewDate = Convert.ToDateTime(reader["ReviewDate"]),
+                                ReviewRating = Convert.ToInt32(reader["ReviewRating"]),
+                                ReviewDescription = reader["ReviewDescription"].ToString()
+                            };
+                            existingProduct.Reviews.Add(review);
+                        }
+                    }
                 }
             }
 
             // Bind the products to the Repeater control
             rptProducts.DataSource = products;
             rptProducts.DataBind();
+        }
+
+        protected void rptProducts_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                var product = (Product)e.Item.DataItem;
+
+                // Find the nested repeater
+                Repeater rptReviews = (Repeater)e.Item.FindControl("rptReviews");
+                if (rptReviews != null && product.Reviews.Count > 0)
+                {
+                    rptReviews.DataSource = product.Reviews;
+                    rptReviews.DataBind();
+                }
+            }
         }
 
         protected void btnBuyNow_Click(object sender, EventArgs e)
@@ -75,6 +128,16 @@ namespace NewVersion.css
         public string ProductImageURL { get; set; }
         public decimal Price { get; set; }
 
+         public List<Review> Reviews { get; set; }
+
+    }
+
+    public partial class Review
+    {
+        public string ReviewID { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public int ReviewRating { get; set; }
+        public string ReviewDescription { get; set; }
     }
 
 }
