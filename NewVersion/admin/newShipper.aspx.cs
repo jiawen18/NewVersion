02f011dc8cc36ee1ecf510d3d0a96f36dfbb5ca8
goldfizh <chongyj-wg17@student.tarc.edu.my>
2024-09-26 16:52:48 +0800

    updatealskdngflsa kjdfgba ogahe;olu ha;ofbl

--- NewVersion/admin/newShipper.aspx.cs
+++ NewVersion/admin/newShipper.aspx.cs
@@ -30,26 +30,12 @@ namespace NewVersion.admin
 
         protected void Page_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
-=======
-
-            if (!IsPostBack)
-            {
-                BindGrid();
-            }
-        }
-
->>>>>>> 86cf3bc8bcf3431942c093f919979914f331d4a2
             if (!IsPostBack)
             {
 
             }
         }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 86cf3bc8bcf3431942c093f919979914f331d4a2
         private void BindGrid()
         {
             using (var context = new userEntities())
@@ -72,11 +58,7 @@ namespace NewVersion.admin
             {
                 using (var context = new userEntities())
                 {
-<<<<<<< HEAD
-                    // Create a new shipper
-=======
 
->>>>>>> 86cf3bc8bcf3431942c093f919979914f331d4a2
                     var shipper = new Shipper
                     {
                         shipperName = addName.Text,
@@ -85,29 +67,15 @@ namespace NewVersion.admin
                         shipperAddress = addAddress.Text
                     };
 
-<<<<<<< HEAD
-                    // Add the new shipper to the database
-                    context.Shippers.Add(shipper);
-                    context.SaveChanges();
-
-                    // Store the newly added shipper in the session list
-                    AddedShippers.Add(shipper);
-
-                    // Only show the newly added shipper in the GridView
-                    GridView1.DataSource = new List<Shipper> { shipper };
-                    GridView1.DataBind();
-
-                    // Clear the input fields
-=======
                     context.Shippers.Add(shipper);
                     context.SaveChanges();
 
                     AddedShippers.Add(shipper);
 
+
                     GridView1.DataSource = new List<Shipper> { shipper };
                     GridView1.DataBind();
 
->>>>>>> 86cf3bc8bcf3431942c093f919979914f331d4a2
                     ClearFields();
 
                     FeedbackLabel.Text = "Shipper added successfully!";
@@ -128,11 +96,7 @@ namespace NewVersion.admin
                     var shipper = context.Shippers.Find(shipperID);
                     if (shipper != null)
                     {
-<<<<<<< HEAD
-                        // Populate fields for editing
-=======
 
->>>>>>> 86cf3bc8bcf3431942c093f919979914f331d4a2
                         addName.Text = shipper.shipperName;
                         addEmail.Text = shipper.shipperEmail;
                         addPhone.Text = shipper.shipperPhone;
