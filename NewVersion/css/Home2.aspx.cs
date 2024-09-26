c63908f84d76ee18907cec77090f1c8772cf2b44
jiawen18 <162662193+jiawen18@users.noreply.github.com>
2024-09-26 18:57:51 +0800

    sqsq

--- NewVersion/css/Home2.aspx.cs
+++ NewVersion/css/Home2.aspx.cs
@@ -4,19 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace NewVersion.css
 {
     public partial class Home2 : System.Web.UI.Page
     {
+        string cs = Global.CS;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                productRepeater.DataSource = SqlDataSource1;
+                productRepeater.DataBind();
+                BindProductData();
+            }
         }
 
         protected void btnBuyNow_Click(object sender, EventArgs e)
         {
             Response.Redirect("ProductDetails.aspx");
+        }
+
+        private void BindProductData()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionString"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = @"
+                    SELECT 
+                        p.ProductID, 
+                        p.ProductImageURL, 
+                        p.ProductName, 
+                        p.Price, 
+                        AVG(r.ReviewRating) AS AverageRating 
+                    FROM 
+                        Product p 
+                    LEFT JOIN 
+                        Review r ON p.ProductID = r.ProductID 
+                    WHERE 
+                        p.IsVisible = 1 
+                    GROUP BY 
+                        p.ProductID, 
+                        p.ProductImageURL, 
+                        p.ProductName, 
+                        p.Price";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    productRepeater.DataSource = reader;
+                    productRepeater.DataBind();
+                }
+            }
         }
     }
 }
--- NewVersion/css/Smartphones.aspx.designer.cs
+++ NewVersion/css/Smartphones.aspx.designer.cs
@@ -31,23 +31,5 @@ namespace NewVersion.css
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptProducts;
-
-        /// <summary>
-        /// Repeater1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater1;
-
-        /// <summary>
-        /// Repeater3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater Repeater3;
     }
 }
