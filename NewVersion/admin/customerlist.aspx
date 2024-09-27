dc318e0932465abcd11e848d4432d7ef2d3f1e9a
Chong391 <kelvinchong0457@gmail.com>
2024-09-27 18:28:30 +0800

    db

---
+++ NewVersion/admin/customerlist.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="customerlist.aspx.cs" Inherits="NewVersion.customerlist" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="main" runat="server">
+        <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table
+                        id="add-row"
+                        class="display table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>Customer ID</th>
+                                <th>Email</th>
+                                <th>Username</th>
+                                <th style="width: 10%">Action</th>
+                            </tr>
+                        </thead>
+                          <tbody>
+                   <!-- Repeater control to bind customer data -->
+                    <asp:Repeater ID="RepeaterCustomerList" runat="server">
+                        <ItemTemplate>
+                            <tr>
+                                <td><%# Eval("Username") %></td>
+                                <td>
+                                    <asp:Label ID="lbl_customerID" runat="server" Text='<%# Eval("MemberID") %>'></asp:Label>
+                                    <asp:TextBox ID="txt_customerID" runat="server" Text='<%# Eval("MemberID") %>' CssClass="form-control" Visible="false"></asp:TextBox>
+                                </td>
+                                <td>
+                                    <asp:Label ID="lbl_email" runat="server" Text='<%# Eval("Email") %>'></asp:Label>
+                                    <asp:TextBox ID="txt_email" runat="server" Text='<%# Eval("Email") %>' CssClass="form-control" Visible="false"></asp:TextBox>
+                                </td>
+                                 <td>
+                                    <asp:Label ID="lbl_username" runat="server" Text='<%# Eval("Username") %>'></asp:Label>
+                                    <asp:TextBox ID="txt_username" runat="server" Text='<%# Eval("Username") %>' CssClass="form-control" Visible="false"></asp:TextBox>
+                                </td>
+                                <td>
+                                    <div class="form-button-action">
+                                        <!-- Edit button -->
+                                        <asp:Button ID="btnEdit" runat="server" Text="Edit" CssClass="btn btn-link btn-primary btn-lg"
+                                            CommandName="EditCustomer" CommandArgument='<%# Eval("Username") %>' OnClick="EditCustomer_Click" />
+
+                                        <!-- Save button, initially hidden -->
+                                        <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn btn-link btn-success btn-lg"
+                                            CommandName="SaveCustomer" CommandArgument='<%# Eval("Username") %>' OnClick="SaveCustomer_Click" Visible="false" />
+
+                                        <!-- Delete button with confirmation -->
+                                        <asp:Button ID="btnDelete" runat="server" Text="Delete" CssClass="btn btn-link btn-danger btn-lg"
+                                            CommandName="DeleteCustomer" CommandArgument='<%# Eval("Username") %>' OnClick="DeleteCustomer_Click"
+                                            OnClientClick="return confirm('Are you sure you want to delete this admin?');" />
+                                    </div>
+                                </td>
+                            </tr>
+                        </ItemTemplate>
+                    </asp:Repeater>
+                </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ NewVersion/admin/customerlist.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NewVersion
+{
+    public partial class customerlist : System.Web.UI.Page
+    {
+        public partial class update : System.Web.UI.Page
+        {
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!IsPostBack)
+                {
+                    LoadCustomerData();
+                }
+            }
+
+
+            // Method to load admin data and bind it to the Repeater control
+            private void LoadCustomerData()
+            {
+                string connString = ConfigurationManager.ConnectionStrings["productConnectionString"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string query = "SELECT MemberID, Email, Username FROM MemberUser";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        RepeaterCustomerList.DataSource = reader;
+                        RepeaterCustomerList.DataBind();
+                    }
+                }
+            }
+
+            // Event handler for the "Edit" button to enable editing
+            protected void EditCustomer_Click(object sender, EventArgs e)
+            {
+                RepeaterItem item = (sender as Button).NamingContainer as RepeaterItem;
+
+                // Toggle visibility of the TextBox and Label controls
+                Label lbl_username = item.FindControl("lbl_username") as Label;
+                TextBox txt_username = item.FindControl("txt_username") as TextBox;
+
+                Label lbl_email = item.FindControl("lbl_email") as Label;
+                TextBox txt_email = item.FindControl("txt_email") as TextBox;
+
+                // Hide labels and show TextBoxes for editing
+                lbl_username.Visible = false;
+                txt_username.Visible = true;
+
+                lbl_email.Visible = false;
+                txt_email.Visible = true;
+
+                // Show the Save button and hide the Edit button
+                Button btnEdit = item.FindControl("btnEdit") as Button;
+                Button btnSave = item.FindControl("btnSave") as Button;
+
+                btnEdit.Visible = false;
+                btnSave.Visible = true;
+            }
+
+            // Event handler for saving updated admin details
+            protected void SaveCustomer_Click(object sender, EventArgs e)
+            {
+                string username = (sender as Button).CommandArgument;
+                RepeaterItem item = (sender as Button).NamingContainer as RepeaterItem;
+
+                // Get updated values from TextBoxes
+                TextBox txt_username = item.FindControl("txt_username") as TextBox;
+                TextBox txt_email = item.FindControl("txt_email") as TextBox;
+
+                string newUsername = txt_username.Text;
+                string newEmail = txt_email.Text;
+
+                string connString = ConfigurationManager.ConnectionStrings["productConnectionString"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string updateQuery = "UPDATE MemberUser SET Username = @Username, Email = @Email WHERE Username = @Username";
+                    using (SqlCommand cmd = new SqlCommand(updateQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", newUsername);
+                        cmd.Parameters.AddWithValue("@Email",newEmail);
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                // Reload the customer data to reflect the changes
+                LoadCustomerData();
+            }
+
+            // Event handler for deleting an admin account
+            protected void DeleteCustomer_Click(object sender, EventArgs e)
+            {
+                string username = (sender as Button).CommandArgument;
+
+                string connString = ConfigurationManager.ConnectionStrings["productConnectionString"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    string deleteQuery = "DELETE FROM AdminUser WHERE Username = @Username";
+                    using (SqlCommand cmd = new SqlCommand(deleteQuery, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                // Reload the admin data to reflect the changes
+                LoadCustomerData();
+            }
+        }
+    }
+}
